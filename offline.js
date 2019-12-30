2dc142eb48c0123d0d7d7a0f8f102945a2ed5e06
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 13:23:21 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1577692405,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bullet-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/player-sheet0.png",
+		"images/playbackground-sheet0.png",
+		"images/ghostshooter-sheet0.png",
+		"images/spawnmonsterbottomleft-sheet0.png",
+		"images/hudbest-sheet0.png",
+		"images/hudscore-sheet0.png",
+		"images/hudbar-sheet0.png",
+		"images/hudbarframe-sheet0.png",
+		"images/reload-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
